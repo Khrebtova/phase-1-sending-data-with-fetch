33c2118bd435b3bd444685ba36f1df71767c0a1c
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2021-12-20 12:48:12 -0800

    completed 2 out of 3

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const user = {
+        name: `${userName}`,
+        email: `${userEmail}`,
+    }
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(user => {
+        document.querySelector("body").innerHTML = `${user.id}`        
+    })
+    .catch((error) => {
+        let message = "Unauthorized Access"
+        document.querySelector("body").innerHTML = `${error.message} reason: ${message}`
+        console.log(error.message)
+    })
+   }
+submitData('Jim', 'jim@jim.com')
